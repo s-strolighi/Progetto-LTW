88df90224df8a7a51cff26ef5acbbe9522951bd8
christian <chri.grifoni@gmail.com>
2020-04-26 21:41:57 +0200

    esercizi2

--- js/WorkoutEsercizi.js
+++ js/WorkoutEsercizi.js
@@ -4,22 +4,40 @@
     //filterSelection("all")
     function filterSelection(c) 
     {
-    var x, i;
+      var c2=c;
+    var x, i, y, k;
    x = document.getElementsByClassName("filterDiv"); //x = array degli oggetti che hanno la classe filterDiv (tutti quelli che possono essere filtrati)
-   if (c == "all") c = "";
+    if (c == "all") c = "";
+
    // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
    for (i = 0; i < x.length; i++) //per tutti gli oggetti filtrabili
    {
-     w3RemoveClass(x[i], "show");//rimuoviamo a tutti la classe show (quella che permette che siano visibili definita nel file css)
-     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show"); //x[i].className restituisce le classi dell'oggetto attuale
-                                                                   //indexOf ritorna la prima occorrenza della stringa c in quelle classi
+     RemoveClass(x[i], "show");//rimuoviamo a tutti la classe show (quella che permette che siano visibili definita nel file css)     
+     if (x[i].className.indexOf(c) > -1)
+       {
+          AddClass(x[i], "show"); //x[i].className restituisce le classi dell'oggetto attuale
+        }                                                           //indexOf ritorna la prima occorrenza della stringa c in quelle classi
                                                                    //poi verifica se la stringa ritornata è >-1 (in pratica se c'è stato un riscontro)
                                                                    //se è cosi allora aggiungo la classe show che permette di mostrare l'oggetto
-   }
- }
+    }
  
+ //faccio la stessa cosa con la classe active dei bottoni cosi da evidenziare solo quello appena premuto
+ y = document.getElementsByClassName("btn");   
+  
+    for(k = 0; k < y.length; k++)
+       {
+        RemoveClass(y[k], "active");
+        if (y[k].className.indexOf(c2) > -1)
+          {
+             AddClass(y[k], "active"); 
+          }
+      }
+
+
+    }//chiusura funzione
+
  // Show filtered elements
- function w3AddClass(element, name) {
+ function AddClass(element, name) {
    var i, arr1, arr2;
    arr1 = element.className.split(" ");
    arr2 = name.split(" ");
@@ -31,7 +49,7 @@
  }
  
  // Hide elements that are not selected
- function w3RemoveClass(element, name) {
+ function RemoveClass(element, name) {
    var i, arr1, arr2;
    arr1 = element.className.split(" ");
    arr2 = name.split(" ");
@@ -52,7 +70,9 @@
      current[0].className = current[0].className.replace(" active", "");
      this.className += " active";
    });
- 
+
+
+
  
  }
      
