6796bc441f0994b4ab49948d6ca55e5671bb9a9e
Lorenzo <lorenzo.capobianchi@gmail.com>
2020-05-29 00:52:51 +0200

    Aggiunti commenti ai file associati a 'Fai da te'

--- js/HobbyFaiDaTe.js
+++ js/HobbyFaiDaTe.js
@@ -3,24 +3,30 @@ $(document).ready(function(){
     var arrayLink=$(".sidebar-button"); //tutti i button della sidebar
     var lastLink=arrayLink[0]; //primo button della sidebar
 
+    //assegno un numero ad ogni sezione che serve per creare una corrispondenza tra sezione e bottone
     var contatore=0;
     $(".sezione").each(function(){
         $(this).prop("numero",contatore);
         contatore++;
     })
 
+    //il click sul primo link va gestito separatamente per portare la pagina all'inizio
     $("#primo-link").click(function(){
             window.scrollTo({top : 0});  //Per scrollare la versione per pc
             document.getElementsByClassName("contenitore-sezioni")[0].scrollTo({top : 0});  //per scrollare la versione per telefono
     });
 
+    //gestisco lo scroll di due elementi diversi (window e la div "contenitore-sezioni") nel seguente modo:
+    // 1) se sono al pc (la larghezza della finesta è maggiore di 750px) gestisco lo scroll della finestra
+    // 2) altrimenti se sono al telefono gestisco lo scroll della div "contenitore-sezioni"
     if (window.innerWidth > 750){
         $(window).scroll(function(){
-            //var elementi=document.getElementsByClassName("sezione");
             var newElem;
-            var actualHeight=window.pageYOffset;
-            var windowHeight=window.innerHeight*40/100; //20% dell'altezza della finestra (escluso barre del browser)
-            var window80=window.innerHeight*80/100;
+            var actualHeight=window.pageYOffset;    //offset attuale della pagina
+            var windowHeight=window.innerHeight*40/100; //40% dell'altezza della finestra (usato per la sottolineatura del bottone giusto)
+            var window80=window.innerHeight*80/100;     //80% dell'altezza della finesta  (usato per l'effetto di fadeIn)
+            //alla fine di questo "ciclo" avrò trovato l'elemento al quale mettere la sottolineatura
+            //e se non era già visibile lo avrò reso visibile
             $(".sezione").each(function(){
                     if ($(this).offset().top <= actualHeight + windowHeight){
                         newElem = $(this);
@@ -29,12 +35,18 @@ $(document).ready(function(){
                         $(this).fadeTo(500,1);
                     }
             });
+            //rimuovo la classe "underlined" dall'elemento che era precedentemente sottolineato
+            //e inserisco la classe "underlined" all'elemento che è entrato nella visuale per ultimo
+            // (la classe "underlined" sottolinea il testo contenuto dall'elemento)
+            //infine aggiorno l'ultimo elemento
             lastLink.classList.remove("underlined");
             newLink=arrayLink[newElem.prop("numero")];
             newLink.classList.add("underlined");
             lastLink=newLink;
         });
     }
+    // stessa cosa fatta per il l'evento scroll dell'elemento window ora lo faccio per l'evento scroll
+    //dell'elemento "contenitore-sezioni"
     else {
         $(".contenitore-sezioni").scroll(function(){
             var newElem;
@@ -54,36 +66,7 @@ $(document).ready(function(){
             lastLink=newLink;
         });
     }
-    $(".contenitore-sezioni").scroll();
-/*
-    $(window).scroll(function() {
-        $(".fadeIn").each(function(){
-            var actualHeight=window.pageYOffset;
-            var windowHeight=window.innerHeight*80/100;
-            if ($(this).css("opacity")==0 && $(this).offset().top <= actualHeight + windowHeight){
-                $(this).fadeTo(500,1);
-            }
-        });
-    }).scroll();*/
-
-/*
-    $(window).scroll(function() { //aggiungi un event handler dell'evento scroll    
-        var windowBottom = $(this).scrollTop() + $(this).innerHeight(); //height al posto di innerHeight?  dall'inizio al punto più alto attuale + altezza del documento (praticamente l'altezza totale del documento?)
-        $(".fadeIn").each(function() {
-            // Check the location of each desired element 
-            var objectBottom = $(this).offset().top + $(this).outerHeight(); //offset ci da la posizione relativa al documento + l'altezza dell'elemento
-            console.log($(this).offset().top);
-            // If the element is completely within bounds of the window, fade it in 
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-            } //else { //object goes out of view (scrolling up)
-                //if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-            //}
-        });
-    }).scroll(); //invoke scroll-handler on page-load
-*/
-
-
+    $(".contenitore-sezioni").scroll(); //chiamo la funzione scroll per rendere visibile il primo elemento al caricamento della pagina
 });
 
 
