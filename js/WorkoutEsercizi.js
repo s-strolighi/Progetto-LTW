168b43a0c4dafce57c064c8e1f27cf2a48d340a5
christian <chri.grifoni@gmail.com>
2020-06-01 17:39:16 +0200

    revisione sezione workout

--- js/WorkoutEsercizi.js
+++ js/WorkoutEsercizi.js
@@ -1,72 +1,114 @@
-//filterSelection("all")
-function filterSelection(c) {
+function filterSelection(c) // il parametro c è il nome del bottone che ha triggerato la funzione. io devo filtrare tutte le card che che hanno quella stessa classe. questo filtra viene eseguito solo tra le card che hanno la classe filterDiv
+{
+  buttons = document.getElementById("nav-hide").getElementsByClassName("btn"); //elenco dei bottoni nella seconda navbar
+  filters = []; //elenco delle classi dei bottoni attivi
 
-  buttons = document.getElementById("nav-hide").getElementsByClassName("btn"); //elenco dei bottoni
-  filters = []; //elenco dei filtri attualmente attivi
+  if (c == "tutti") c = ''; //se premo il bottone tutti imposto il parametro in modo da attivare tutte le card
 
-  if (c == "tutti") c = '';
 
+  //PARTE DI GESTIONE DEI BOTTONI DOPO L'ONCLICK:
+   
   //solo se c == "tutti" li setto tutti disattivati
-  for (var i = 0; i < buttons.length; i++) {
-    if (buttons[i].className.indexOf(c) > -1) {
-      if (buttons[i].className.indexOf('active') > -1 || c == '') { //attivo bottone se ha quel filtro o se "tutti" è attivo
-        buttons[i].classList.remove("active");
-      } else {
-        buttons[i].classList.add("active");
+  for (var i = 0; i < buttons.length; i++)         //scorro tutti i bottoni del filtra
+  {
+    //indexOf ritorna -1 se non trova occorrenze di c. quindi questo if è falso se il bottone non ha la classe corrispondente a quella del bottone premuto (c)
+    if (buttons[i].className.indexOf(c) > -1)      //quindi questo controllo lo passa solo 1 bottone. quello premuto
+    { 
+      if (buttons[i].className.indexOf('active') > -1 || c == '') //se il bottone premuto era gia attivo o se èstato premuto il bottone tutti (e quindi devono essere disattivati tutti gli altri) lo disattivo
+      { 
+        buttons[i].classList.remove("active");  //rimuovo la classe active
+      } 
+      else //se invece il bottone premuto non era attivo lo attivo
+      {
+        buttons[i].classList.add("active"); //
       }
 
     }
 
     //salvo le classi dei bottoni attivi come filtri attualmente attivi
-    if (buttons[i].className.indexOf('active') > -1 && buttons[i].className.indexOf('tutti') < 0) {
-      filters[filters.length] = getFilter(buttons[i]); //aggiungo a filters[] tutte le classi dei bottoni attivi
+    if (buttons[i].className.indexOf('active') > -1 && buttons[i].className.indexOf('tutti') < 0) //se il bottone attuale è attivo e non è il bottone tutti
+    {
+      filters[filters.length] = getFilter(buttons[i]); //aggiungo a filters[] tutte le classi dei bottoni attivi. 
+                                                       //getFilter si assicura che nell'elenco delle classi non vengano 
+                                                       //aggiunte anche le classi btn e active, che sono classi dei
+                                                       // bottoni ma non quelle che ci interessano.
     }
-  }
 
-  //se non ho filtri attivi (tutte le schede), attivo il primo bottone
-  if (filters.length == 0) {
+
+  }//fine for
+
+  //se non ho filtri attivi (tutte le schede), attivo il primo bottone (quello con tutti)
+  if (filters.length == 0) 
+  {
     buttons[0].classList.add("active");
-  } else {
+  } 
+  else 
+  {
     buttons[0].classList.remove("active");
   }
 
-  cards = document.getElementsByClassName("filterDiv"); //x = array degli oggetti che hanno la classe filterDiv (tutti quelli che possono essere filtrati)
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+
+
+
+  //PARTE DI GESTIONE DELLE CARD DA VISUALIZZARE OPPURE NO DOPO LA GESTIONE DEI BOTTONI
+
+  //prendo tutte le carte che hanno la classe filterDiv (tutti quelli che possono essere filtrati)
+  cards = document.getElementsByClassName("filterDiv");
+  
+  
   for (i = 0; i < cards.length; i++) //per tutti gli oggetti filtrabili
   {
-    card_class = cards[i].className.split(' '); //tutte le classi della cards
+    card_class = cards[i].className.split(' '); //qui dentro metto le classi della card attuale. che possono essere piu 
+                                                 //di una visto che una card puo avere due classi nel caso in cui 
+                                                 //esercizio sia di 2 o piu tipi (esempio esercizi gambe e cardio 
+                                                 //corrispondono a schede con classi gambe e cardio)
 
     // !IMPORTANTE! every controlla che TUTTE le occorrenze matchano, SOME controlla che almeno una matcha
-    if (filters.some(filtro => card_class.indexOf(filtro) > -1) || filters.length == 0) { //controllo che i filtri selezionati nei bottoni sono presenti all'interno delle calssi della cards
-      //se la card deve essere vista
-      cards[i].classList.add("show");
-    } else {
-      cards[i].classList.remove("show");
+    
+    //controllo che ci sia almeno una corrispondenza tra filters(che è l'elenco dei bottoni attivi) e card_class(che è l'elenco delle classi della carta attuale)
+    //se c'è almeno una corrispondenza allora la carta va resa visibile
+    if (filters.some(filtro => card_class.indexOf(filtro) > -1) || filters.length == 0)  //anche se filters è vuoto (cioe se è selezionato i lbottone tutti) la card attuale va resa visibile
+    { 
+      cards[i].classList.add("show"); //allora la rendo visibile
+    } 
+    else 
+    {
+      cards[i].classList.remove("show");//altrimenti la rendo invisibile
     }
   }
 
 } //chiusura funzione
 
 //Restituisce la classe del bottone inerente agli esercizi (toglie active e btn dalle classi)
-function getFilter(c) {
-  classes = c.className.split(' ');
-  classes = classes.filter(e => e !== 'btn').filter(e => e !== 'active');
-  return (classes[0]);
+function getFilter(c) 
+{
+  classes = c.className.split(' '); /*splitta il contenuto della variabile class del parametro passato*/
+  classes = classes.filter(e => e !== 'btn').filter(e => e !== 'active'); /*restituisci la classe solo se la classe non è active o btn*/
+  return (classes[0]);  /*restituisco la prima delle classi rimanenti. Anche se per come la useremo noi rimarra sempre una classe sola*/
 }
 
-// movimento navbar del filtra
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
 
-  position = document.getElementById("nav-fixed").clientHeight;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav-hide").style.top = position+"px";
-  } else {
-    document.getElementById("nav-hide").style.top = "-"+position+"px";
+//GESTIONE MOVIMENTO DELLA NAVBAR DEL FILTRA
+
+//metto in una variabile il numero di pixel che distanziano la pagina come è visualizzata in 
+//questo momento dalla pagina visualizzata inizialmente. cioe prima di scrollare.
+var prevScrollpos = window.pageYOffset; 
+
+window.onscroll = function () //funzione triggerata quando scrollo la finestra
+ {
+  var currentScrollPos = window.pageYOffset; //aggiorno la posizione della finestra mentre scrollo
+
+  position = document.getElementById("nav-fixed").clientHeight; //restituisce altezza della navbar fissa compreso il padding
+
+  if (prevScrollpos > currentScrollPos) //quando scorro verso l'alto
+  {
+    document.getElementById("nav-hide").style.top = position+"px"; //aumento la distanza della navbar fissa con il top della finestra. di esattamente l'altezza della navbar mobile
+  } 
+  else
+  {
+    document.getElementById("nav-hide").style.top = "-"+position+"px";//diminuisco la distanza della navbar fissa con il top della finestra di un valore pari alla sua altezza. essendo, la navbar mobile, a un livello inferiore rispetto a quella fissa, questa verrà coperta. 
   }
-  prevScrollpos = currentScrollPos;
+  prevScrollpos = currentScrollPos; //aggiorno costantemente la nuova posizione della finestra
 }
 
 
-//mostro la barra all'interno delle card solo se il testo va in overflow
